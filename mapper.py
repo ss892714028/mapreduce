c775a2ea16b8a16b9dcabc2afe0b6105b320d078
ss892714028 <star.0731@163.com>
2021-03-13 02:36:30 -0500

    from server

--- mapper.py
+++ mapper.py
@@ -1,13 +1,20 @@
-#!bin/python
+#!/bin/python
+
 
 import sys
 
+SEPARATOR = '\t'
 
-def read(std_input):
+
+def read_input(std_input):
     for line in std_input:
         yield line.strip()
 
+def main(separator='\t'):
+    data = read_input(sys.stdin)
+    for words in data:
+        print('{}{}{}'.format(words, separator, 1))
 
 if __name__ == "__main__":
-    url = read(sys.stdin)
-    print('{}\t{}'.format(url, 1))
+    main()
+
--- reducer.py
+++ reducer.py
@@ -1,24 +1,25 @@
-#!bin/python
+#!/bin/python
 
 import sys
 from itertools import groupby
+from operator import itemgetter
 
 SEPARATOR = '\t'
 
 
 def read(std_in):
     for line in std_in:
-        yield line.strip().split(SEPARATOR, 1)
+        yield line.rstrip().split(SEPARATOR, 1)
 
 
 def main():
     data = read(sys.stdin)
-    for curr, group in groupby(data):
+    for curr, group in groupby(data,itemgetter(0)):
         try:
-            total_count = 0
-            for curr, count in group:
-                total_count += count
+	    total_count = sum(int(count) for curr, count in group)
             print('{}{}{}'.format(curr, SEPARATOR, total_count))
         except ValueError:
             pass
 
+if __name__ == "__main__":
+    main()
